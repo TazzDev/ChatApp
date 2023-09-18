24ecb383176f51724d3ba04183c4ac1ab53f7327
Thomson <thomsonbingo@gmail.com>
2023-09-18 19:38:27 +0530

    Button constant

--- src/components/ConversationList/ConversationList.styled.ts
+++ src/components/ConversationList/ConversationList.styled.ts
@@ -6,14 +6,23 @@ export const ConversationListContainer = styled.div`
   padding: 4px;
   background: white;
   border-radius: 4px;
+  height: 40%;
+  overflow: scroll;
 `;
 
-export const ConversationControls = styled.div``;
+export const ConversationControls = styled.div`
+  margin-bottom: 20%;
+  button {
+    position: fixed;
+    width: 20%;
+  }
+`;
 
 export const ConversationListWrapper = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
+  justify-content: center;
   width: 40%;
   padding: 0 10vw;
   background-color: #ffffff2f;
